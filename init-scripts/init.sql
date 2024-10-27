2cb3c009142559fe0baa95f85d9ef494d5ad9ccf
Justin <justinsiaw00@gmail.com>
2024-10-28 01:03:37 +0800

    Update SQL script

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,11 +1,14 @@
-CREATE TABLE IF NOT EXISTS clicks (
-    id SERIAL PRIMARY KEY,
-    clicked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    mailer_id  TEXT NOT NULL REFERENCES mailers(id),
-) PARTITION BY RANGE (clicked_at);
-
+DROP TABLE IF EXISTS clicks; 
+DROP TABLE IF EXISTS mailers;
 
 CREATE TABLE IF NOT EXISTS mailers (
     id TEXT PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS clicks (
+    id SERIAL,
+    clicked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    mailer_id TEXT NOT NULL REFERENCES mailers(id),
+    PRIMARY KEY (id, clicked_at)  -- Include both id and clicked_at in the primary key
+) PARTITION BY RANGE (clicked_at);
